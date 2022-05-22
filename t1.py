2ceb76d5e9135e98cf22aafd1b9237d475cb6761
boy030 <boy301222@gmail.com>
2022-05-22 14:18:23 +0800

    annotation

--- t1.py
+++ t1.py
@@ -1,11 +1,34 @@
-from donkeycar.parts.controller import LocalWebController, JoystickController, WebFpv,JoyStickPub
+from donkeycar.parts.controller import LocalWebController, JoystickController, WebFpv,JoyStickPub 
+from donkeycar.parts.ominibot_car_com import OminibotCar,threading,sys 
+import os 
+import time 
+import numpy 
 
-import os
-import time
-import numpy
+_port = "/dev/ominibot_car"
+_baud = 115200 
 
-joystick_message = JoyStickPub()
-while True:
-  time_data = int(time.time())
-  message_data=joystick_message.run()
+ominibot = OminibotCar(_port, _baud) 
+ominibot.set_system_mode(platform="individual_wheel") 
+
+a=80
+
+joystick_message = JoyStickPub() 
+while True: 
+  time_data = int(time.time()) 
+  message_data=joystick_message.run() 
   print("message_data=",message_data)
+  print(message_data[3])
+  if  message_data[2]=="left_stick_vert" and message_data[3]>0 :
+      ominibot.individual_wheel(V1=a,V3=a,V2=a,V4=a, debug=False) 
+      time.sleep(1)
+  elif message_data[2]=="left_stick_vert" and message_data[3]<0 :
+      ominibot.individual_wheel(V1=-a,V3=-a,V2=-a,V4=-a, debug=False) 
+      time.sleep(1)
+      
+  elif message_data[2]=="left_stick_horz" and message_data[3]<0 :
+        ominibot.individual_wheel(V1=0,V3=0,V2=a,V4=a, debug=False) 
+        time.sleep(1) 
+        
+  elif message_data[2]=="left_stick_horz" and message_data[3]>0 :
+        ominibot.individual_wheel(V1=a,V3=a,V2=0,V4=0, debug=False) 
+        time.sleep(1) 
--- t2.py
+++ t2.py
@@ -1,24 +1,30 @@
-from donkeycar.parts.ominibot_car_com import OminibotCar,threading,sys
-import time
+from donkeycar.parts.ominibot_car_com import OminibotCar,threading,sys 
+import time 
 
 _port = "/dev/ominibot_car"
-_baud = 115200
-ominibot  = OminibotCar(_port,_baud)
-ominibot.set_system_mode(platform="individual_wheel")
+_baud = 115200 
+
+ominibot = OminibotCar(_port, _baud) 
+ominibot.set_system_mode(platform="individual_wheel") 
 
 a=80
 
-ominibot.individual_wheel(V1=a,V3=a,V2=a,V4=a, debug=False)
-time.sleep(2)
+#w
+ominibot.individual_wheel(V1=a,V3=a,V2=a,V4=a, debug=False) 
+time.sleep(2) 
 
-ominibot.individual_wheel(V1=a,V3=a,V2=0,V4=0, debug=False)
-time.sleep(2)
+#d
+ominibot.individual_wheel(V1=a,V3=a,V2=0,V4=0, debug=False) 
+time.sleep(2) 
 
-#ominibot.individual_wheel(V1=0,V3=0,V2=a,V4=a, debug=False)
-time.sleep(2)
+#a
+ominibot.individual_wheel(V1=0,V3=0,V2=a,V4=a, debug=False) 
+time.sleep(2) 
 
-ominibot.individual_wheel(V1=0,V3=0,V2=0,V4=0, debug=False)
-time.sleep(2)
+#s 
+ominibot.individual_wheel(V1=0,V3=0,V2=0,V4=0, debug=False) 
+time.sleep(2) 
 
-ominibot.individual_wheel(V1=-a,V3=-a,V2=-a,V4=-a, debug=False)
+#ss
+ominibot.individual_wheel(V1=-a,V3=-a,V2=-a,V4=-a, debug=False) 
 time.sleep(2)
